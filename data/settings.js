5385fe947d724c51b2298b10ddd4ad174df39e24
alexsaimanimation-cmd <alexsaimanimation@gmail.com>
2026-01-19 01:08:44 +0600

    Add application configuration settings

---
+++ data/settings.js
@@ -0,0 +1,15 @@
+window.APP_CONFIG = {
+    "logo_url": "https://blogger.googleusercontent.com/img/a/AVvXsEgaL3kqKDy9eVoJqP9ag70kr6X6btraBx082uSl6viSxcXJ70T9hBH6BP59WwuStKa6NVvurScncqfBdgaDgE4y_DR4I1ldX_O05tM_O9NCIoz9j6CmZvTiC3nyp3JNN7w89J4COVvCEKFFC1zmMUmrkaUsScm9HPKUXyz65qW2m_fiB2mkvlf3mgEe2eb2=s1600",
+    "app_name": "StreamHub",
+    "show_logo": true,
+    "placeholder_url": "https://placehold.co/1280x720/e0e0e0/333333?text=No+Image",
+    "splash_icon": "https://cdn-icons-png.flaticon.com/512/2889/2889676.png",
+    "admin_splash_icon": "",
+    "unlock_enabled": true,
+    "unlock_links": [
+        "https://www.google.com",
+        "https://www.bing.com"
+    ],
+    "unlock_duration_minutes": 60,
+    "unlock_countdown_seconds": 8
+};
